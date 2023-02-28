01450acc8e7326889dca5b0a2f07a0c525d5d863
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-02-28 19:04:52 +0100

    fetch function tested

--- Evernet test/index.js
+++ Evernet test/index.js
@@ -1,4 +1,14 @@
-const API_URL = {
-    url: 'https://api.baserow.io/api/database/rows/table/114016/',
-    apiKey: 'WF4G7N2yKjDHpGVUaPoVqmxBgkCHGNxh'
-}
+const token = 'WF4G7N2yKjDHpGVUaPoVqmxBgkCHGNxh';
+const tableId = 114016;
+
+fetch(`https://api.baserow.io/api/database/rows/table/${tableId}/`, {
+    headers: {
+        'Authorization': `Token ${token}`,
+        'Content-Type': 'application/json'
+    },
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+
