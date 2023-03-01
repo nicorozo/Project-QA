93f3862643def2120e351c7849352a23357e042d
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-03-01 15:49:32 +0100

    validation update

--- Evernet test/index.js
+++ Evernet test/index.js
@@ -10,6 +10,8 @@ const industryName = document.getElementById('fld_industry')
 const fld_phone = document.getElementById('fld_phone')
 const facebookUrl = document.getElementById('fld_fb_page')
 
+const elementsArray = [fullName, companyName, fld_email, companyUrl, industryName, fld_phone, facebookUrl]
+
 const token = 'WF4G7N2yKjDHpGVUaPoVqmxBgkCHGNxh';
 const tableId = 114016;
 
@@ -27,8 +29,42 @@ fetch(`https://api.baserow.io/api/database/rows/table/${tableId}/`, {
 
 
 
-createRecordButton.addEventListener('click', function () {
+createRecordButton.addEventListener('click', function (e) {
 
+    e.preventDefault()
 
-    console.log(fullName)
+    const validatedArray = elementsArray.map((element) => validateInputs(element))
+
+    console.log(validatedArray)
 })
+
+function errorHandling(element, message) {
+
+    element.placeholder = message
+
+    element.classList.add('error')
+
+}
+function success(element) {
+    element.classList.remove('error')
+}
+
+
+function validateInputs(element) {
+
+    // const fullNameValue = fullName.value.trim()
+    // const companyNameValue = companyName.value.trim()
+    // const fld_emailValue = fld_email.value.trim()
+    // const companyUrlValue = companyUrl.value.trim()
+    // const industryNameValue = industryName.value.trim()
+    // const fld_phoneValue = fld_phone.value.trim()
+    // const facebookUrlValue = facebookUrl.value.trim()
+    console.log(element.name)
+    if (element.value.trim() === '') {
+        errorHandling(element, `${element.type} is required`)
+        return false
+    } else {
+        success(element)
+        return element.value.trim()
+    }
+}
