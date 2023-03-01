75b38c74ae8a34f5e9b6802f1e1943741cdb2ef6
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-03-01 10:51:33 +0100

    input id and label, JS set up

--- Evernet test/index.js
+++ Evernet test/index.js
@@ -1,5 +1,18 @@
+
+const createRecordButton = document.getElementById('create-button')
+const deleteRecordButton = document.getElementById('delete-button')
+
+const fullName = document.getElementById('fld_fullname').value
+const companyName = document.getElementById('fld_company_name').value
+const fld_email = document.getElementById('fld_email').value
+const companyUrl = document.getElementById('fld_company_url').value
+const industryName = document.getElementById('fld_industry').value
+const fld_phone = document.getElementById('fld_phone').value
+const facebookUrl = document.getElementById('fld_fb_page').value
+
 const token = 'WF4G7N2yKjDHpGVUaPoVqmxBgkCHGNxh';
 const tableId = 114016;
+
 
 fetch(`https://api.baserow.io/api/database/rows/table/${tableId}/`, {
     headers: {
@@ -12,3 +25,10 @@ fetch(`https://api.baserow.io/api/database/rows/table/${tableId}/`, {
     .catch(error => console.error(error));
 
 
+
+
+createRecordButton.addEventListener('click', function () {
+
+
+    console.log(fullName)
+})
