b83551278662a3d716c1e3c95dcb090965d4574b
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2023-03-01 10:53:26 +0100

    value deleted on JS

--- Evernet test/index.js
+++ Evernet test/index.js
@@ -2,13 +2,13 @@
 const createRecordButton = document.getElementById('create-button')
 const deleteRecordButton = document.getElementById('delete-button')
 
-const fullName = document.getElementById('fld_fullname').value
-const companyName = document.getElementById('fld_company_name').value
-const fld_email = document.getElementById('fld_email').value
-const companyUrl = document.getElementById('fld_company_url').value
-const industryName = document.getElementById('fld_industry').value
-const fld_phone = document.getElementById('fld_phone').value
-const facebookUrl = document.getElementById('fld_fb_page').value
+const fullName = document.getElementById('fld_fullname')
+const companyName = document.getElementById('fld_company_name')
+const fld_email = document.getElementById('fld_email')
+const companyUrl = document.getElementById('fld_company_url')
+const industryName = document.getElementById('fld_industry')
+const fld_phone = document.getElementById('fld_phone')
+const facebookUrl = document.getElementById('fld_fb_page')
 
 const token = 'WF4G7N2yKjDHpGVUaPoVqmxBgkCHGNxh';
 const tableId = 114016;
